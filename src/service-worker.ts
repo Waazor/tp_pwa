ed956c15001777f54ea0692c500bdb72cdc59cff
Victor_WILLOTEAUX <victorwilloteaux@gmail.com>
2025-02-06 11:44:30 +0100

    Manifest + service worker

---
+++ src/service-worker.ts
@@ -0,0 +1,7 @@
+self.addEventListener('install', (_event) => {
+  console.log('Service Worker installé.');
+});
+
+self.addEventListener('activate', (_event) => {
+  console.log('Service Worker activé.');
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,37 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'PWA',
+        short_name: 'PWA',
+        description: 'Une PWA',
+        theme_color: '#007bff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+      workbox: {
+        clientsClaim: true,
+        skipWaiting: true,
+      },
+    }),
+  ],
+});
